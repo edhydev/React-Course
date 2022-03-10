72864b9f2fe1a6dfe8897db722129827ce8883cc
edhydev <edgar.jimenez.dev@gmail.com>
2022-03-10 12:05:36 -0600

    Class Component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const FruitCard = (props) => {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <hr />
-      <p>$ {props.price}</p>
-    </div>
-  );
-};
+class FruitCard extends React.Component {
+  constructor() {
+    super();
+
+    this.add = this.add.bind(this);
+    this.reduce = this.reduce.bind(this);
+
+    this.state = {
+      count: 0,
+    };
+  }
+
+  add = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  reduce = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>{this.props.name}</h1>
+        <p>Count: {this.state.count}</p>
+        <hr />
+        <button onClick={this.add}>+</button>
+        <button onClick={this.reduce}>-</button>
+        <p>$ {this.props.price}</p>
+      </div>
+    );
+  }
+}
 
 const App = () => {
   return (
